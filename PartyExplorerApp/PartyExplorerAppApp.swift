f998d88fb51b2895fa973973ea49ecaaa5c57812
Sanjay Mali <malisanjay5433@gmail.com>
2023-12-04 18:35:10 +0530

    setup: intial commit

---
+++ PartyExplorerApp/PartyExplorerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyExplorerAppApp.swift
+//  PartyExplorerApp
+//
+//  Created by Sanjay Mali on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PartyExplorerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
